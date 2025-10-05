c45d8b1fc7434d5816492b710ef5370fada10b1b
SofiiaLashyna <soflashina297@gmail.com>
2025-10-05 15:47:57 +0300

    fifth commit - added header Galaxy

---
+++ Galaxy.h
@@ -0,0 +1,36 @@
+#ifndef GALAXY_H
+#define GALAXY_H
+#include <vector>
+#include "CelestialObjects.h"
+#include "Graph.h"
+
+class StarSystem {
+private:
+    Star centralStar;
+    std::vector<Planet> planets;
+public:
+    StarSystem(int id, Star& star);
+
+    void addPlanet(Planet& planet);
+    void removePlanet(std::string& name);
+
+    Star getStar();
+    std::vector<Planet> getPlanets();
+
+    void showSystem();
+    int getId();
+};
+class Galaxy {
+private:
+    Graph<> systemGraph;
+    std::vector<StarSystem> systems;
+public:
+    Galaxy();
+
+    void addSystem(StarSystem& system);
+    void connectSystems(int id1, int id2);
+
+    void generateRandomGalaxy(int numSystems);
+    void showGalaxy();
+};
+#endif //GALAXY_H
